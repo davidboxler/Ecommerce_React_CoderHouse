ad272a8aad6b8775d7fbff3ed148d2cb22c348ea
David Boxler <david.boxler@incluit.com>
2022-09-15 15:34:54 -0300

    se agregan cambios proyecto

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,10 +4,10 @@ import './Hero.scss'
 const Hero = () => {
   return (
     <section id='hero'>
-        <h4>Trade-in-offer</h4>
-        <h2>Super value deals</h2>
-        <h1>On all products</h1>
-        <p>Save more with coupnos & up to with 70!</p>
+        <h4>En un solo lugar</h4>
+        <h2>Todo lo que buscas</h2>
+        <h1>Al mejor precio</h1>
+        <p>Aprovecha mas de 70% de descuento</p>
         <button className='normal'>Saber más</button>
     </section>
   )
